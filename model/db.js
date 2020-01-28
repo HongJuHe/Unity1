2a93f8a9ed8c42d7f662382d988a4b1fd08b8c00
HongJuHe <jhhong1102@naver.com>
2020-01-27 20:18:58 -0800

    add upload page

--- model/db.js
+++ model/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
-var dbURI = 'mongodb://localhost:27017/';
+var dbURI = 'mongodb://juhee:1234@localhost:27017/admin';
 if (process.env.NODE_ENV === 'production') {
     dbURI = process.env.MONGOLAB_URI;
 }
