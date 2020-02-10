51503076b2fd8cc9d0dc9245881924e8644402db
Dayoung1014 <wsqqa1014@gmail.com>
2020-02-10 02:52:57 -0800

    upload17

--- controllers/play.js
+++ controllers/play.js
@@ -4,17 +4,6 @@ var fs = require('fs');
 var objectid =0;
 
 /* GET 'Upload' page*/
-module.exports.play = function(req, res) {
-    res.render('play', {
-        title: 'play game',
-        pageHeader: {
-            title: 'Unity'},
-        pageFooter: {
-            explain: 'copylight'}
-    });
-};
-
-/* GET 'Upload' page*/
 module.exports.playgame = function(req, res){
    //var objectid = req.params.objectid;
    objectid = req.query.objectid || req.params.objectid;
@@ -35,11 +24,11 @@ module.exports.playgame = function(req, res){
          
    if(results) 
 	{
+
 		var password = results[0]._doc.password;
 		Filedb.find({"password":{$eq:password}}, function(err, results) 
 		{
    		if (err) {
-      		//console.err('error :' + err.stack);
      	 	console.error(err);
       		res.writeHead('200', {'Content-Type' : 'text/html;charset=utf8'});
      	 	res.write('<h2>error error</h2>');
@@ -49,66 +38,65 @@ module.exports.playgame = function(req, res){
      	 	}
          
   	 	if(results) {
+
+		var output = [];
 		var game_name = results[0]._doc.game_name;
-		var name = 0;
-		var id = 0;
 		var uploadAt = results[0]._doc.uploadAt;
 		var content = results[0]._doc.content;
 		var password = results[0]._doc.password;
+		var name = results[0]._doc.name;
+		var id = results[0]._doc.id;
 
-		console.dir(results);
-      		res.writeHead('400', {'Content-Type' : 'text/html;charset=utf8'});
-		//change test 
-      		res.write('<h2>Play Game</h2>');
-
-     	 for (var i = 0 ; i < results.length; i++)
+		var dir = "public/fileStorage/" +password;
+		var htmldir = " ";
+		var files = fs.readdirSync(dir);
+	
+      		for (var i = 0; i < files.length ; i++)
 		{
-        	 //game_name
-		if(i==0)
-            	res.write('<p>game_name : ' +game_name +'</p>');
-         	
-        	 //name(id)
-           	 name = results[i]._doc.name;
-           	 id = results[i]._doc.id;
-            	if(i == 0)
-            		res.write('<p>name: ');  
-            	res.write(name +'(' +id +') ');  
-     		 } 
-		res.write('</p>'); 
+			var file = files[i];
+			var suffix = file.substr(file.length -5, file.length);
+			console.log(suffix);
 
-      for (var i = 0 ; i < results.length; i++)
-	{
-	if(i==0){
-         //uploadAt, content
-           res.write('<p>uploadAt : ' +uploadAt +'</p>');
-           res.write('<p>content : ' +content +'</p>');
+			if (suffix == '.html') 
+			{
+				htmldir = file; 		
+			}
 		}
-    	 } 
+		htmldir = '/fileStorage/' +password +'/' +htmldir;
+		
+		console.log(name.length)
+		for (var i = 0 ; i < name.length; i++) 
+		{
 
-      var dir = "public/fileStorage/" +password;
-      var htmldir = " ";
+			name = results[0]._doc.name[i];
+			id = results[0]._doc.id[i];
 
-      var files = fs.readdirSync(dir);
-	console.log(files);
-
-      for (var i = 0; i < files.length ; i++){
-	var file = files[i];
-	var suffix = file.substr(file.length -5, file.length);
-	console.log(suffix);
-
-	if (suffix == '.html') {
-		htmldir = file; 		
-	}
-}
-	
-	htmldir = '/fileStorage/' +password +'/' +htmldir;
-	//res.write(htmldir);
-	res.write("<iframe src = " +htmldir +" style='width:80%; height:70%'></iframe>");
-	
-      res.end();
-        }
+			output.push({game_name : game_name, uploadAt : uploadAt, content : content, password : password, htmldir : htmldir, name : name, id : id});
+		}
+		
+		console.log(output);
+		//res.write(htmldir);
+		res.render('play', {
+		title: 'play game',
+		result : output,
+		pageHeader: {
+		title: 'Unity'},
+		pageFooter: {
+		explain: 'copylight'}
+    });		
+}   
         });
-         }
-});
-}
+	} //2ndresult
+       
+       
+//res.end();
+	else 
+    {
+		res.write('game loading failed');
+		res.end();
+	}
+       
+   });
+   };
+
 
